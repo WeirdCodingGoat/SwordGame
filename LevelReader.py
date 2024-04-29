353e1cf476bbcf4c7f0820e6cdf29faab3ff41a3
WeirdCodingGoat <iggy@Mac-mini.local>
2024-04-29 00:15:32 -0500

    + non-functioning level-reader +Level_test

--- LevelReader.py
+++ LevelReader.py
@@ -14,28 +14,67 @@ class Platform(pygame.sprite.Sprite):
         self.solid = True
         self.semisolid = True
 
-def load_level(level):
-    levels=open("level_maps.txt","r")
-    header=(level*20)+1
+def load_level(selected):
+    with open("level_maps.txt") as level:
+        levels = level.read()
+    header=(selected*20)+1
+
+    levels=levels.split()[header:header+20]
+    level.close()
+    level=[[]]
+    print(levels)
+    for spot in levels:
+        print(spot,len(level))
+        if len(level[-1]) == 40:
+            level.append([spot])
+        else:
+            level[-1].append(spot)
+
+    print(level)
     platforms=[]
     platform=[]
     cordinates=[]
+    check=1
 
-    for row in 20:
-        for character in 40:
-            if len(cordinates) == 0 and levels.readline(row)(character) == "-": 
+    for row in range(20): # add a start and a stop (start:header) (stop=header+20)
+        for character in range(40):
+            print("row",row,"char",character)
+            if len(cordinates) == 0 and level[row-1][character-1] == "-": 
                 #bug might appear when program reads cordinates and adds more objects when reading 
                 #a line with filler for a block.
                 cordinates.append((character,row))
-            elif len(cordinates) >0 and not levels.readline(row)(character) == "-":
+
+            elif len(cordinates) >0 and not level[row-1][character-1] == "-" and cordinates[-1]!="end":
                 cordinates.append("end") # Maybe replace this with another flag/word?
 
+        for land in platforms:
+            for item in cordinates:  
+                if item[0] >= land[0][0] and item[0] <= land[1][0] and item[1] >= land[0][1] and item[1] <= land[1][1]:
+                    cordinates.pop(item)
 
+        for cord in cordinates:
+            if cord != "end":
+                platform.append(cordinates.pop(cordinates.index(cord))) # Might need to make a condition where it 
+                # makes sure that it's appending a full platform. Using the information provided
+                # by checking each layer.
+
+            else:
+                for segment in platform: # Finding the lowest Y cord for platform.
+                    check=1
+                    while segment[1] + check != " ":
+                        check+=1
+                    if type(platforms[-1]) == type(1): #should result in null if none in list.
+                        if check < platforms[-1]:
+                            platforms[-1]=check # Might get an assignment error.
+                    else:
+                        platforms.append(check)
+                platforms.append([platform[0],(platform[-1][0],platforms.pop(check))])
+                platform=[]
         # look in platforms to not make duplicates (use inequalities), 
         # Append cordinates to "platform" down to the bottom of the platform,
         # Take the first and the last cordinates and append them to platforms as [(x1,y1),(x2,y2)]
         # enemy format = "["Enemy type",(x,y)]" 
-            
-        platforms.append(cordinates)
         cordinates=[]
+    print(platforms)
 
+load_level(0)
